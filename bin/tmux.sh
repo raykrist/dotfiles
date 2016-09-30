fd8880500ccbb6abc0bdeaede1371215bd7926b8
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-09-30 14:29:51 +0200

    Updated tmux

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd ${HOME}/dev/norcams/himlar
+cd ${HOME}
 
 name=$1
 
 SESSION=`basename $PWD`
 
 tmux new-session -d -s $name
-tmux rename-window -t $name:1 himlar
+tmux rename-window -t $name:1 login
 tmux new-window -t $name -a -n cli
-tmux new-window -t $name -a -n master
-tmux new-window -t $name -a -n dboard
-tmux new-window -t $name -a -n compute
+tmux new-window -t $name -a -n os-cli
+tmux new-window -t $name -a -n id
+tmux new-window -t $name -a -n dashboard
 tmux select-window -t $name:1
 tmux attach -t $name
 
