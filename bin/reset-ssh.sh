68027b8bc6d26dea5321b9e461287dc4f2e7a21b
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-06-27 10:20:48 +0200

    Updated tmux.sh

---
+++ bin/reset-ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ~/bin/ssh-find-agent.sh
+ssh-find-agent -a
+
--- bin/tmux.sh
+++ bin/tmux.sh
@@ -10,8 +10,6 @@ tmux new-session -d -s $name
 tmux rename-window -t $name:1 login
 tmux new-window -t $name -a -n cli
 tmux new-window -t $name -a -n os-cli
-tmux new-window -t $name -a -n id
-tmux new-window -t $name -a -n dashboard
 tmux select-window -t $name:1
 tmux attach -t $name
 
