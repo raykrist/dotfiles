f4584ed51b9b6d875382c4dfb726032472d79e42
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2019-04-05 10:48:16 +0000

    Updated tmux script

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -7,8 +7,8 @@ name=$1
 SESSION=`basename $PWD`
 
 tmux new-session -d -s $name
-tmux rename-window -t $name:1 login
-tmux new-window -t $name -a -n cli
+tmux rename-window -t $name:1 ansible
+tmux new-window -t $name -a -n tf
 tmux new-window -t $name -a -n os-cli
 tmux select-window -t $name:1
 tmux attach -t $name
